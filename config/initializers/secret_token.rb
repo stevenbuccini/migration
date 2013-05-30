df5851cdb9e90d69a7d647e47ba93bba3c44b0ae
Alex Triana <alex.triana2@gmail.com>
2013-05-30 10:51:42 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Iceman::Application.config.secret_token = 'ad14bc283108e61befbae751e181680b2c06a2a7b06b2851daa1ed19fa138445a96de8a317cca3aaf8c6b4be0d8fae326351c0e043b3536ec44f7c00472944fb'
